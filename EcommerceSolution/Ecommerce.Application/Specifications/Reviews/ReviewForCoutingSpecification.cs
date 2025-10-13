77955c0a71c31d685fe324d4cfc519d871955ce7
Angel Bustamante <bustamanteangel532@gmail.com>
2025-10-13 11:45:28 -0600

    t-administracion: feat(reviews): implementación de administración y paginación de reviews y comentarios
    - Agregado mantenimiento y creación de reviews
    - Endpoint para eliminación de reviews por administrador
    - Implementada paginación con CQRS para listar reviews
    - Pruebas de ejecución y validación de paginación completadas

---
+++ EcommerceSolution/Ecommerce.Application/Specifications/Reviews/ReviewForCoutingSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Ecommerce.Domain;
+
+namespace Ecommerce.Application.Specifications.Reviews
+{
+    public class ReviewForCoutingSpecification : BaseSpecification<Review>
+    {
+        public ReviewForCoutingSpecification(ReviewSpecificationParams reviewParams)
+            : base(
+                  x => 
+                  (!reviewParams.ProductId.HasValue || x.ProductId == reviewParams.ProductId)
+            )
+        {
+
+        }
+    }
+}
