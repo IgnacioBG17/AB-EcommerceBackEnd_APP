ffbec5cad85ec9801760c61b23e8eff6ef069e0a
Angel Bustamante <bustamanteangel532@gmail.com>
2025-10-14 13:24:32 -0600

    t-administracion: feat(ShoppingCart): implementa administración completa del carrito de compras
    - Creación y generación de carritos asociados a usuarios o sesiones
    - Implementación de controlador y endpoints para gestión de carritos
    - Actualización de ítems en el carrito (agregar, modificar y eliminar)
    - Integración con UnitOfWork y AutoMapper para persistencia y proyección
    - Retorno de ViewModels con estructura ordenada de productos

---
+++ EcommerceSolution/Ecommerce.Api/Controllers/ShoppingCartController.cs
@@ -0,0 +1,68 @@
+﻿using Ecommerce.Application.Features.ShoppingCarts.Commands.DeleteShoppingCartItem;
+using Ecommerce.Application.Features.ShoppingCarts.Commands.UpdateShoppingCart;
+using Ecommerce.Application.Features.ShoppingCarts.Queries.GetShoppingCartById;
+using Ecommerce.Application.Features.ShoppingCarts.Vms;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Ecommerce.Api.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public ShoppingCartController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// Obtiene el carrito de compras asociado al identificador especificado.
+        /// Si el identificador es vacío (<see cref="Guid.Empty"/>), genera un nuevo carrito
+        /// y devuelve su estructura inicial, un objeto <see cref="ShoppingCartVm"/> con la información del carrito existente
+        /// o un nuevo carrito vacío si no se encontró ninguno.
+        /// </summary>
+        /// <returns>
+        /// </returns>
+        [AllowAnonymous]
+        [HttpGet("{id}", Name = "GetShoppingCart")]
+        [ProducesResponseType(typeof(ShoppingCartVm), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartVm>> GetShoppingCart(Guid id)
+        {
+            var shoppingCartId = id == Guid.Empty ? Guid.NewGuid() : id;
+            var query = new GetShoppingCartByIdQuery(shoppingCartId);
+            return await _mediator.Send(query);
+        }
+
+        /// <summary>
+        /// Este método actualiza el carrito de compras de un usuario.
+        //  Elimina los productos anteriores y agrega los nuevos que vienen en la petición.
+        //  (Reemplaza completamente los ítems del carrito con los nuevos productos seleccionados.)
+        /// </summary>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPut("{id}", Name = "UpdateShoppingCart")]
+        [ProducesResponseType(typeof(ShoppingCartVm), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartVm>> UpdateShoppingCart(Guid id, UpdateShoppingCartCommand request)
+        {
+            request.ShoppingCartId = id;
+            return await _mediator.Send(request);
+        }
+
+        /// <summary>
+        /// Elimina un ítem del carrito de compras identificado en el comando
+        /// y devuelve el carrito actualizado con sus ítems ordenados por producto.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpDelete("item/{id}", Name = "DeleteShoppingCart")]
+        [ProducesResponseType(typeof(ShoppingCartVm), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<ShoppingCartVm>> DeleteShoppingCart(int id)
+        {
+            return await _mediator.Send(new DeleteShoppingCartItemCommand() { Id = id});
+        }
+    }
+}
---
+++ EcommerceSolution/Ecommerce.Application/Features/ShoppingCarts/Queries/GetShoppingCartById/GetShoppingCartByIdHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Ecommerce.Application.Features.ShoppingCarts.Vms;
+using Ecommerce.Application.Persistence;
+using Ecommerce.Domain;
+using MediatR;
+using System.Linq.Expressions;
+
+namespace Ecommerce.Application.Features.ShoppingCarts.Queries.GetShoppingCartById
+{
+    public class GetShoppingCartByIdHandler : IRequestHandler<GetShoppingCartByIdQuery, ShoppingCartVm>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public GetShoppingCartByIdHandler(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<ShoppingCartVm> Handle(GetShoppingCartByIdQuery request, CancellationToken cancellationToken)
+        {
+            var includes = new List<Expression<Func<ShoppingCart, object>>>();
+            includes.Add(p => p.ShoppingCartItems!.OrderBy(x => x.Producto));
+
+            var shoppingCart = await _unitOfWork.Repository<ShoppingCart>().GetEntityAsync(
+                x => x.ShoppingCartMasterId == request.ShoppingCartId,
+                includes,
+                true // que lo mantenga en memoria
+            );
+
+            if (shoppingCart is null)
+            {
+                shoppingCart = new ShoppingCart 
+                { 
+                    ShoppingCartMasterId = request.ShoppingCartId,
+                    ShoppingCartItems = new List<ShoppingCartItem>()
+                };
+
+                _unitOfWork.Repository<ShoppingCart>().AddEntity(shoppingCart);
+                await _unitOfWork.Complete();
+            }
+
+            return _mapper.Map<ShoppingCartVm>(shoppingCart);
+        }
+    }
+}
